ef2d830bc254bf7f07d8bf314341bd4640c635e9
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2022-12-20 17:01:32 +0100

    refactor: switched from browserRouter to HashRouter

--- src/ui/routes/routes.js
+++ src/ui/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from 'ui/components/pages/home/component';
 import NavigationHeader from 'ui/components/shared/navigationHeader';
 import BPMNViewer from 'ui/components/pages/processView/component';
