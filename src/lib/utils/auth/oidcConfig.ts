1b811deb2dfb05b375b207587a7a496f4ea70f02
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-07-24 10:26:18 +0200

    chore: oidc conf

--- src/lib/utils/auth/oidcConfig.ts
+++ src/lib/utils/auth/oidcConfig.ts
@@ -1,25 +1,25 @@
-// export const buildOidcConfiguration = ({ oidcConf }) => {
-//   const { origin } = window.location;
-//   return {
-//     ...oidcConf,
-//     redirect_uri: `${origin}/protools/authentication-v2/callback`,
-//     post_logout_redirect_uri: `${origin}/`,
-//     silent_redirect_uri: `${origin}/protools/authentication-v2/silent_callback`,
-//   };
-// };
+export const buildOidcConfiguration = ({ oidcConf }) => {
+  const { origin } = window.location;
+  return {
+    ...oidcConf,
+    redirect_uri: `${origin}/orchestration-protocole-enquete/authentication-v2/callback`,
+    post_logout_redirect_uri: `${origin}/`,
+    silent_redirect_uri: `${origin}/orchestration-protocole-enquete/authentication-v2/silent_callback`,
+  };
+};
 
-// export const buildOidcConfigurationFromKeycloak = ({ keycloakConf }) => {
-//   const { origin } = window.location;
-//   const { realm, 'auth-server-url': authServer, resource } = keycloakConf;
-//   return {
-//     authority: `${authServer}/realms/${realm}`,
-//     client_id: resource,
-//     redirect_uri: `${origin}/protools/authentication-v2/callback`,
-//     response_type: 'code',
-//     post_logout_redirect_uri: `${origin}/`,
-//     scope: 'openid profile email',
-//     silent_redirect_uri: `${origin}/protools/authentication-v2/silent_callback`,
-//     automaticSilentRenew: true,
-//     loadUserInfo: true,
-//   };
-// };
+export const buildOidcConfigurationFromKeycloak = ({ keycloakConf }) => {
+  const { origin } = window.location;
+  const { realm, 'auth-server-url': authServer, resource } = keycloakConf;
+  return {
+    authority: `${authServer}/realms/${realm}`,
+    client_id: resource,
+    redirect_uri: `${origin}/orchestration-protocole-enquete/authentication-v2/callback`,
+    response_type: 'code',
+    post_logout_redirect_uri: `${origin}/`,
+    scope: 'openid profile email',
+    silent_redirect_uri: `${origin}/orchestration-protocole-enquete/authentication-v2/silent_callback`,
+    automaticSilentRenew: true,
+    loadUserInfo: true,
+  };
+};
