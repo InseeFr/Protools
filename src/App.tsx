ad980bfa149ea7071898b0f38614a6a3db2762a6
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2023-09-22 16:34:58 +0200

    refactor: oidc provider

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { RouterProvider } from 'react-router-dom';
 import { getConfig } from './lib/hooks/getConfig';
 import Router from './lib/routes/router';
 import ErrorBoundary from './components/shared/layout/ErrorBoundary';
+import AuthProvider from './lib/utils/provider/authProvider';
 
 import './App.css';
 
@@ -15,57 +16,61 @@ const queryClient = new QueryClient();
 function App() {
   const [configuration, setConfiguration] = useState(false);
   const [apiUrl, setApiUrl] = useState<string>('');
-  const [keycloakAuth, setKeycloakAuth] = useState<boolean>(false);
+  const [keycloakAuth, setKeycloakAuth] = useState<string>('basic');
+  const [identityProvider, setIdentityProvider] = useState<string>('');
 
   if (!configuration) {
     getConfig().then((data: any) => {
       console.log('Fetched config');
       setConfiguration(true);
       setApiUrl(data.API_URL);
-      setKeycloakAuth(data.KEYCLOAK_AUTH_URL == 'oidc');
+      setKeycloakAuth(data.KEYCLOAK_AUTH_TYPE);
+      setIdentityProvider;
     });
   }
 
   return (
     <MuiDsfrThemeProvider>
-      <QueryClientProvider client={queryClient} contextSharing>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Header
-          //brandTop={<>RÉPUBLIQUE FRANÇAISE</>}
-          homeLinkProps={{
-            href: '/',
-            title: 'Protools - Accueil',
-          }}
-          operatorLogo={{
-            alt: 'Logo Insee',
-            imgUrl: '/insee.png',
-            orientation: 'vertical',
-          }}
-          quickAccessItems={[
-            {
-              iconId: 'fr-icon-admin-line',
-              linkProps: {
-                href: '/',
-              },
-              text: 'FakeUser',
-            },
-          ]}
-          serviceTagline="Orchestration des protocoles d'enquêtes"
-          serviceTitle="Protools"
-          brandTop={undefined}
-        />
-        <ErrorBoundary>
-          <div
-            style={{
-              // margin: 'auto',
-              // maxWidth: 1000,
-              ...fr.spacing('padding', { topBottom: '10v' }),
+      <AuthProvider authType={keycloakAuth} identityProvider={identityProvider}>
+        <QueryClientProvider client={queryClient} contextSharing>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Header
+            //brandTop={<>RÉPUBLIQUE FRANÇAISE</>}
+            homeLinkProps={{
+              href: '/',
+              title: 'Protools - Accueil',
             }}
-          >
-            <RouterProvider router={Router} />
-          </div>
-        </ErrorBoundary>
-      </QueryClientProvider>
+            operatorLogo={{
+              alt: 'Logo Insee',
+              imgUrl: '/insee.png',
+              orientation: 'vertical',
+            }}
+            quickAccessItems={[
+              {
+                iconId: 'fr-icon-admin-line',
+                linkProps: {
+                  href: '/',
+                },
+                text: 'FakeUser',
+              },
+            ]}
+            serviceTagline="Orchestration des protocoles d'enquêtes"
+            serviceTitle="Protools"
+            brandTop={undefined}
+          />
+          <ErrorBoundary>
+            <div
+              style={{
+                // margin: 'auto',
+                // maxWidth: 1000,
+                ...fr.spacing('padding', { topBottom: '10v' }),
+              }}
+            >
+              <RouterProvider router={Router} />
+            </div>
+          </ErrorBoundary>
+        </QueryClientProvider>
+      </AuthProvider>
     </MuiDsfrThemeProvider>
   );
 }
